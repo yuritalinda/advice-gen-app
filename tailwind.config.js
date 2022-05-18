de82763106c0f206969cd554bd2e297b83e54ddc
Yuritalinda <yuritalinda@gmail.com>
2022-05-18 11:01:59 -0400

    configurando estilos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "light-cyan": "#CEE3E9",
+      "neon-green": "#52FFA8",
+      "grayish-blue": "#4E5D73",
+      "dark-grayish-blue": "#323A49",
+      "dark-blue": "#1F2632",
+    },
+    fontFamily: {
+      body: ["Manrope"],
+    },
+
     extend: {},
   },
   plugins: [],
-}
+};
